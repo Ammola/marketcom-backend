dbcfbe69647f2df7e3e152f4ea8def65cb45311c
Ammola <ooammolaoo@gmail.com>
2021-02-19 16:27:23 +0300

    first commit

---
+++ src/main/java/com/ga/marketcom/MarketcomApplication.java
@@ -0,0 +1,13 @@
+package com.ga.marketcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/marketcom/MarketcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.marketcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
